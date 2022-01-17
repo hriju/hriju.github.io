5008261a007ade90750cef291a565e8f97873a7e
Mahbub <hriju@Outlook.com>
2022-01-17 05:41:05 -0600

    Decorate my text

---
+++ DecorateMyText/js/decoratemytext.js
@@ -0,0 +1,26 @@
+var flag=0;
+function biggerText(){
+    if (flag == 0){
+        document.getElementById("textval").style.fontSize = "24pt";
+         flag=1;
+    }
+    else{
+        document.getElementById("textval").style.fontSize = "";
+         flag=0;
+    }
+}
+const checkbox = document.getElementById('checkbox')
+function changecolor()
+{
+  if (document.getElementById('checkbox').checked) 
+  {
+      document.getElementById("textval").style.fontWeight="bold";
+      document.getElementById("textval").style.color="green";
+      document.getElementById("textval").style.textDecoration="underline";
+  } else {
+      document.getElementById("textval").style.color="black";
+      document.getElementById("textval").style.textDecoration="";
+      document.getElementById("textval").style.fontWeight="";
+      
+  }
+}
