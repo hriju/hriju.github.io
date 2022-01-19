51c30c8c8568a9216b3ef5ea6dd9998fd54da14c
Mahbub <hriju@Outlook.com>
2022-01-19 14:51:21 -0600

    asciianimation

---
+++ asciiAnimation/js/homework8ascii.js
@@ -0,0 +1,74 @@
+var spe=250;
+var timer=null;
+var index=0;
+var text="";
+
+window.onload=function(){
+    able(true);
+    var animation=document.getElementById("Animation");
+    animation.onchange=aniChange;
+    var size=document.getElementById("size");
+    size.onchange=sizeChange;
+    var start=document.getElementById("start");
+    start.onclick=clickStart;
+    var stop=document.getElementById("stop");
+    stop.onclick=clickStop;
+    stop.disabled=true;
+    var speed=document.getElementById("speed");
+    speed.onchange=speedChange;
+    
+};
+function speedChange(){
+    
+    if(this.checked){
+        spe =50;
+    }
+    else{
+        spe= 250;
+    }
+}
+function able(flag){
+    var stop=document.getElementById("stop");
+    stop.disabled=flag;
+    var start=document.getElementById("start");
+    start.disabled=!flag;
+    var animation=document.getElementById("Animation");
+    animation.disabled=!flag;
+}
+function clickStart(){
+    var animation=document.getElementById("Animation");
+    var contents=ANIMATIONS[animation.value];
+    var box=document.getElementById("box");
+    text=box.value;
+    var arr=contents.split("=====\n");
+    var i=0;
+    var len=arr.length;
+    able(false);
+    timer=setInterval(frames,spe,arr,len);
+}
+function clickStop()
+{
+    var box=document.getElementById("box");
+    box.value=text;
+    text="";
+    clearInterval(timer);
+    timer=null;
+    index=0;
+    able(true);
+}
+function frames(arr,len){
+    var box=document.getElementById("box");
+    box.value=arr[(index%len)];
+    index++;  
+}
+
+function aniChange(){
+    var animation=document.getElementById("Animation");
+    var box=document.getElementById("box");
+    box.value=ANIMATIONS[animation.value];
+}
+function sizeChange(){
+    var size=document.getElementById("size");
+    var box=document.getElementById("box");
+    box.style.fontSize=size.value;    
+}
